9727d55d606f4e90cc4ee050dc969e9468d26e16
_Algorithm_Kid98 <30970161+immanuelsavio@users.noreply.github.com>
2018-12-09 23:58:52 +0530

    made changes

--- identify_face.py
+++ identify_face.py
@@ -3,7 +3,7 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
-data_path = "face_images/"
+data_path = "faceimages/"
 onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
 
 Training_Data, Labels = [], []
